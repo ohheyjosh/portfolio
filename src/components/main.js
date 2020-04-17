31df803d0b91a62386b57e4b23d019cdef809392
Josh Barnett <oh@heyjo.sh>
2020-04-17 17:16:35 -0400

    De-bragging a bit

--- src/components/main.js
+++ src/components/main.js
@@ -53,53 +53,6 @@ const Main = () => (
         </div>
       </div>
     </div>
-    <div className="bg-gray-100 pt-12 sm:pt-16">
-      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl leading-9 font-bold text-gray-900 sm:text-4xl sm:leading-10">
-            I've been at it for a little while now.
-          </h2>
-          <p className="mt-3 text-xl leading-7 text-gray-700 sm:mt-4">
-            Roles I've filled in the past range from design to development to customer success.
-          </p>
-        </div>
-      </div>
-      <div className="mt-10 pb-12 bg-white sm:pb-16">
-        <div className="relative">
-          <div className="absolute inset-0 h-1/2 bg-gray-100"></div>
-          <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="max-w-4xl mx-auto">
-              <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
-                <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
-                  <dt className="order-2 text-lg leading-6 font-medium text-gray-700" id="item-1">
-                    years of experience
-                  </dt>
-                  <dd className="order-1 text-5xl leading-none font-bold text-orange-500" aria-describedby="item-1">
-                    13
-                  </dd>
-                </div>
-                <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt className="order-2 text-lg leading-6 font-medium text-gray-700">
-                    brands launched
-                  </dt>
-                  <dd className="order-1 text-5xl leading-none font-bold text-orange-500">
-                    16
-                  </dd>
-                </div>
-                <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                  <dt className="order-2 text-lg leading-6 font-medium text-gray-700">
-                    happy clients
-                  </dt>
-                  <dd className="order-1 text-5xl leading-none font-bold text-orange-500">
-                    ~40
-                  </dd>
-                </div>
-              </dl>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </main>
 )
 
