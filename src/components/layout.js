1db8c5ed4d9c77dcd8d2d4f5853068d3042b94ec
Josh Barnett <oh@heyjo.sh>
2020-04-21 14:40:46 -0400

    Progress on dark mode toggle switch

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,14 +10,24 @@ const Layout = () => {
     <div className="relative overflow-hidden">
       <ThemeToggler>
         {({ theme, toggleTheme }) => (
-          <label>
+          <div>
             <input
+              id="dark-mode-toggle"
               type="checkbox"
               onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
               checked={theme === 'dark'}
+              aria-checked={theme === 'dark' ? true : false}
+              tabIndex="0"
+            />
+            <label
+              htmlFor="dark-mode-toggle"
+              className="bg-gray-200 relative inline-block flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline"
             />{' '}
-            Dark mode
-          </label>
+            <span
+              aria-hidden="true"
+              className="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200"
+            />
+          </div>
         )}
       </ThemeToggler>
       <Main />
