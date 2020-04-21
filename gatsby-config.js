6cb231020095803d9e4147afd011dab52699346f
Josh Barnett <oh@heyjo.sh>
2020-04-21 14:13:12 -0400

    Dark mode init

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,7 @@ module.exports = {
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
-    }
-  ],
+    },
+    'gatsby-plugin-dark-mode'
+  ]
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
 import Main from "./main"
 import Footer from "./footer"
@@ -7,6 +8,19 @@ import "./layout.css"
 const Layout = () => {
   return (
     <div className="relative overflow-hidden">
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          <label>
+            <input
+              role="checkbox"
+              type="checkbox"
+              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+              checked={theme === 'dark'}
+            />{' '}
+            Dark mode
+          </label>
+        )}
+      </ThemeToggler>
       <Main />
       <Footer />
     </div>
