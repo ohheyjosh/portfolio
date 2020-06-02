1e595254fe518728d468415f741c50353383190e
Josh Barnett <oh@heyjo.sh>
2020-06-02 15:41:14 -0400

    Add link to "header" img

--- src/components/project.js
+++ src/components/project.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import DarkModeToggle from "./dark-mode-toggle"
 import josh from "../images/josh.jpg"
@@ -9,11 +10,13 @@ const ProjectLayout = ({ children }) => {
   return (
     <div className="relative overflow-hidden">
       <DarkModeToggle />
-      <img
-        src={josh}
-        alt="Josh Barnett"
-        className="mt-3 ml-3 w-16 h-16 border-2 shadow-md rounded-full border-solid border-white lg:mt-6 lg:ml-6 lg:w-24 lg:h-24 lg:border-4 lg:shadow-lg"
-      />
+      <Link href="/">
+        <img
+          src={josh}
+          alt="Josh Barnett"
+          className="inline-flex mt-3 ml-3 w-16 h-16 border-2 shadow-md rounded-full border-solid border-white lg:mt-6 lg:ml-6 lg:w-24 lg:h-24 lg:border-4 lg:shadow-lg"
+        />
+      </Link>
       <main className="max-w-screen-xl mx-auto py-4 px-4 overflow-hidden sm:px-6 lg:px-8">
         {children}
       </main>
