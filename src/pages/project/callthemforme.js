4fcdd45751c79cff3ea32af1424b8df6c885d64a
Josh Barnett <oh@heyjo.sh>
2020-06-03 11:27:26 -0400

    CTFM project page progress

--- src/pages/project/callthemforme.js
+++ src/pages/project/callthemforme.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
 import ProjectLayout from "../../components/project"
@@ -8,40 +9,66 @@ const CallThemForMePage = props => (
   <div>
     <SEO title="CallThemForMe, by Josh Barnett" />
     <ProjectLayout>
-      <h1>CallThemForMe.</h1>
-      <p>
-        CallThemForMe connects busy, tired, or anxious people with businesses
-        that run on the phone. If you can't pick up the phone, or just don't
-        want to, let them know and they'll call them for you. With clear
-        pricing, one-step requests, and a free trial call for everyone, there is
-        no excuse to keep anyone waiting for that phone call.
-      </p>
-      <p>
-        Inspired by my wife, who suffers from social anxiety that makes picking
-        up the phone difficult, I spent a month strategizing, designing, and
-        implementing a simple service for people that need a new way to
-        communicate with businesses that run primarily through phone calls.
-      </p>
-      <Img
-        fluid={props.data.ctfmLiveLander.childImageSharp.fluid}
-        alt="CallThemForMe landing page"
-      />
-      <Img
-        fluid={props.data.ctfmRequestFlow.childImageSharp.fluid}
-        alt="CallThemForMe request flow"
-      />
-      <Img
-        fluid={props.data.ctfmMobilePrototype.childImageSharp.fluid}
-        alt="CallThemForMe mobile prototype"
-      />
-      <Img
-        fluid={props.data.ctfmLiveRequest.childImageSharp.fluid}
-        alt="CallThemForMe live request view"
-      />
-      <Img
-        fluid={props.data.ctfmLiveNew.childImageSharp.fluid}
-        alt="CallThemForMe live request form"
-      />
+      <article>
+        <div className="flex flex-col lg:flex-row">
+          <div className="mx-auto w-full py-2 lg:w-1/2 lg:px-4">
+            <h1>CallThemForMe.</h1>
+            <p>
+              CallThemForMe connects busy, tired, or anxious people with
+              businesses that run on the phone. If you can't pick up the phone,
+              or just don't want to, let them know and they'll call them for
+              you. With clear pricing, one-step requests, and a free trial call
+              for everyone, there is no excuse to keep anyone waiting for that
+              phone call.
+            </p>
+            <p>
+              Inspired by my wife, who suffers from social anxiety that makes
+              picking up the phone difficult, I spent a month strategizing,
+              designing, and implementing a simple service for people that need
+              a new way to communicate with businesses that run primarily
+              through phone calls.
+            </p>
+          </div>
+          <div className="mx-auto w-full py-2 lg:w-1/2 lg:px-4">
+            <Img
+              fluid={props.data.ctfmLiveLander.childImageSharp.fluid}
+              alt="CallThemForMe landing page"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <div className="mx-auto w-full py-2 lg:w-1/2 lg:px-4">
+            <Img
+              fluid={props.data.ctfmRequestFlow.childImageSharp.fluid}
+              alt="CallThemForMe request flow"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <div className="mx-auto w-full py-2 lg:w-1/2 lg:px-4">
+            <Img
+              fluid={props.data.ctfmMobilePrototype.childImageSharp.fluid}
+              alt="CallThemForMe mobile prototype"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <div className="mx-auto w-full py-2 lg:w-1/2 lg:px-4">
+            <Img
+              fluid={props.data.ctfmLiveRequest.childImageSharp.fluid}
+              alt="CallThemForMe live request view"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <div className="mx-auto w-full py-2 lg:w-1/2 lg:px-4">
+            <Img
+              fluid={props.data.ctfmLiveNew.childImageSharp.fluid}
+              alt="CallThemForMe live request form"
+            />
+          </div>
+        </div>
+      </article>
     </ProjectLayout>
   </div>
 )
