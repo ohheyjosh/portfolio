f22e81e29a2e3af3bae86533a4b2208a29e90e41
Josh Barnett <oh@heyjo.sh>
2020-06-03 12:22:08 -0400

    Moar margin above the footer

--- src/components/project.js
+++ src/components/project.js
@@ -16,7 +16,7 @@ const ProjectLayout = ({ children }) => {
           className="inline-flex mt-3 ml-3 w-16 h-16 border-2 shadow-md rounded-full border-solid border-white lg:mt-6 lg:ml-6 lg:w-24 lg:h-24 lg:border-4 lg:shadow-lg"
         />
       </Link>
-      <main className="max-w-screen-xl mx-auto py-4 px-4 overflow-hidden sm:px-6 lg:px-8">
+      <main className="max-w-screen-xl mx-auto mb-32 py-4 px-4 overflow-hidden sm:px-6 lg:px-8">
         {children}
       </main>
       <Footer />
