ff48eea307dd04f3cd3a7a4c69866bc93219c93d
Josh Barnett <oh@heyjo.sh>
2020-06-06 11:36:35 -0400

    Other projects listed on project pages

--- src/components/project.js
+++ src/components/project.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import DarkModeToggle from "./dark-mode-toggle"
 import josh from "../images/josh.jpg"
+import ProjectList from "./project-list"
 import Footer from "./footer"
 
 const ProjectLayout = ({ children }) => {
@@ -16,9 +17,10 @@ const ProjectLayout = ({ children }) => {
           className="inline-flex mt-3 ml-3 w-16 h-16 border-2 shadow-md rounded-full border-solid border-white lg:mt-6 lg:ml-6 lg:w-24 lg:h-24 lg:border-4 lg:shadow-lg"
         />
       </Link>
-      <main className="max-w-screen-xl mx-auto mb-32 py-4 px-4 overflow-hidden sm:px-6 lg:px-8 xl:-mt-16">
+      <main className="max-w-screen-xl mx-auto sm:mb-12 py-4 px-4 overflow-hidden sm:px-6 lg:px-8 xl:-mt-16">
         {children}
       </main>
+      <ProjectList />
       <Footer />
     </div>
   )
