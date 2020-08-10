954628ec5fcc3a0c37454386d1ac7e8c55116c79
Josh Barnett <oh@heyjo.sh>
2020-08-10 00:12:47 -0400

    Netlify is handling these links differently...

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -29,7 +29,7 @@ const ProjectList = () => (
           {!location.pathname.includes("callthemforme") && (
             <div className="col-span-1 flex justify-center py-8 px-8">
               <Link
-                to="project/callthemforme"
+                to="/project/callthemforme"
                 className="flex items-center justify-center p-8 rounded-lg shadow-lg w-full hover:shadow-xl transform hover:-translate-y-1 ease-in-out duration-300 focus:outline-none focus:shadow-outline transition"
                 style={{ backgroundColor: "#e1f2fe" }}
               >
@@ -44,7 +44,7 @@ const ProjectList = () => (
           {!location.pathname.includes("skulabs") && (
             <div className="col-span-1 flex justify-center py-8 px-8">
               <Link
-                to="project/skulabs"
+                to="/project/skulabs"
                 className="flex items-center justify-center p-8 rounded-lg shadow-lg w-full hover:shadow-xl transform hover:-translate-y-1 ease-in-out duration-300 focus:outline-none focus:shadow-outline transition"
                 style={{ backgroundColor: "#fafafa" }}
               >
@@ -55,7 +55,7 @@ const ProjectList = () => (
           {!location.pathname.includes("prefix") && (
             <div className="col-span-1 flex justify-center py-8 px-8">
               <Link
-                to="project/prefix"
+                to="/project/prefix"
                 className="flex items-center justify-center p-8 rounded-lg shadow-lg w-full hover:shadow-xl transform hover:-translate-y-1 ease-in-out duration-300 focus:outline-none focus:shadow-outline transition"
                 style={{ backgroundColor: "#ffeb3b" }}
               >
