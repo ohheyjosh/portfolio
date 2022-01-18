84cf62bd26181cc6caa632a1697ca22e90d5c900
Josh Barnett <oh@heyjo.sh>
2022-01-18 10:13:03 -0500

    added redirect page to wordle-wizard project

---
+++ src/pages/wordle-wizard.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from "react"
+
+const WordleWizard = () => {
+  useEffect(() => {
+    window.location.replace("https://wordle-wizard.netlify.app/")
+  })
+
+  return (<></>)
+}
+
+export default WordleWizard
